5662e04582a5123a45ac0628cbc4141e1d731216
yukinasumi <Ireneenriquez657@gmail.com>
2024-09-02 16:54:48 +0000

    9.2(2)第一次在服务器上训练

--- zzy/train.py
+++ zzy/train.py
@@ -7,13 +7,13 @@ import tools
 from matplotlib_inline import backend_inline
 #backend_inline.set_matplotlib_formats('svg')
 import argparse
-
+from torchvision.models import ResNet18_Weights
 
 
 def train_model(epoch,model,device,trainloader,optimizer,creterion,save_path):
     model = model.to(device)
     for epoch in range(epoch):
-        print('epoch {}:'.format(epoch))
+        print('epoch {}:'.format(epoch+1))
         for i,(x, y) in enumerate(trainloader):
             inputs = x.to(device)
             labels = y.to(device)
@@ -45,55 +45,56 @@ def test_model(model,loader,device):
     accuracy = (tp+tn)/(tp+tn+fp+fn)
     precision = tp/(tp+fp) if ((tp+fp)!=0) else 0
     recall = tp/(tp+fn) if ((tp+fn)!=0) else 0
-    print(f'测试集准确率: {100*accuracy}%') 
-    print(f'测试集精确率: {100*precision}%')
-    print(f'测试集召回率: {100*recall}%')
-    print(f'测试集F1值: {(2*precision*recall/(precision+recall)) if ((precision+recall)!=0) else 0}')
+    print(f'accuracy: {100*accuracy}%') 
+    print(f'precision: {100*precision}%')
+    print(f'recall: {100*recall}%')
+    print(f'F1-score: {(2*precision*recall/(precision+recall)) if ((precision+recall)!=0) else 0}')
 
 
 
 if __name__ == '__main__':  
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--save_path',type=str,required=True,help='保存模型的位置')
+    parser.add_argument('--save_path',type=str,required=False,help='保存模型的位置')
     parser.add_argument('--train_path',type=str,required=False)
     parser.add_argument('--test_path',type=str,required=False)
     parser.add_argument('--model_path',type=str,required=False)
-    parser.add_argument('--test',type=bool,required=True)
-    parser.add_argument('--train',type=bool,required=True)
-
+    parser.add_argument('--test',type=bool,required=False,default=False)
+    parser.add_argument('--train',type=bool,required=False,default=False)
+    parser.add_argument('--epoch',type=int,required=False,default=1)
+    
     args = parser.parse_args()
     test = args.test
     train = args.train
     
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(f'device is {device}')
+
 
 
     from torchvision.models import resnet18
-    model = tools.model_modify(resnet18(pretrained=True),2)
+    model = tools.model_modify(resnet18(weights=ResNet18_Weights.DEFAULT),2)
     if args.model_path:
         model.load_state_dict(torch.load(args.model_path))
 
 #hyper params
 
-    epochs = 2
+    epochs = args.epoch
     learning_rate = 0.0005
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     creterion = nn.CrossEntropyLoss()
     batch_size = 10
     save_path = args.save_path
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(f'device is {device}')
 
-    if train:
+
+    if train and args.train_path:
         trainloader = tools.get_loader(args.train_path,batch_size)
         train_model(epochs,model,device,trainloader,optimizer,creterion,save_path)
     
         if save_path:
-            torch.save(model, save_path)
+            torch.save(model.state_dict(), save_path)
         
-    if test:
+    if test and args.test_path:
+        print('test on {}'.format(args.test_path))
         testloader = tools.get_loader(args.test_path,batch_size)
         test_model(model,testloader,device)
